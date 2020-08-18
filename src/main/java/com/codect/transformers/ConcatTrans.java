a833cb340a8117de73793da334ff6230bdd6212b
mordy-arnon <44256831+mordy-arnon@users.noreply.github.com>
2020-08-18 22:50:35 +0300

    Update and rename StringFromFieldsTrans.java to ConcatTrans.java
    improve class.

---
+++ src/main/java/com/codect/transformers/ConcatTrans.java
@@ -0,0 +1,37 @@
+package com.codect.transformers;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+ * add new Field, that is a Concat of String based on DATA, to the Record.
+ * config for example:
+ * {
+ *     "class" : "ConcatTrans",
+ *	"list":[{
+ *     		"fields" : ["KO_ERETZ","_","MISPAR_ZIHUY","_","BANK"],
+ *     		"to":"_id"
+ *      },{
+ *     		"fields" : ["MCH_OBJETCS.","SNIF","_","MCH","_","SUG_LAK"],
+ *     		"to":"innerKey"
+ * 	}]
+ * }
+ */
+public class ConcatTrans extends SingleLineTrans {
+	@Override
+	public Map<String, Object> transLine(Map<String, Object> line) {
+		for(Map<String,Object> concConf:((List<Map<String,Object>>)conf.get("list")){
+			List<String> fields = (List<String>) concConf.get("fields");
+			String newVal = "";
+			for (String col : fields) {
+				object data=DBObjectUtil.getInnerField(col,line);
+				if (data!=null)
+					newVal+=data;
+				else 
+					newVal+=col;
+			}
+			DBObjectUtil.recursivePut((String) concConf.get("to"),line,newVal);
+		}
+		return line;
+	}
+}
