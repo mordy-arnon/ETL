7d0778167017028f5b078151e982f273c5f2d972
mordy-arnon <44256831+mordy-arnon@users.noreply.github.com>
2020-09-01 18:52:46 +0300

    filter transform

---
+++ src/main/java/com/codect/transformers/FilterLineTrans.java
@@ -0,0 +1,20 @@
+package com.codect.transformers;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class FilterLineTrans extends Transformer {
+
+	@Override
+	public List<Map<String, Object>> transform(List<Map<String, Object>> next) {
+		List<Map<String, Object>> result=new ArrayList<>();
+    for (Map<String, Object> line : next) {
+			line = transLine(line);
+      if (line!=null)
+        result.add(line);
+		}
+		return result;
+	}
+
+	public abstract Map<String, Object> transLine(Map<String, Object> line);
+}
